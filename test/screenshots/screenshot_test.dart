3185b777a3677cceaa487d8c7e7a84cf919cd473
nyel <yhiamdan@gmail.com>
2025-03-25 17:07:33 +0000

    Initial Project

---
+++ test/screenshots/screenshot_test.dart
@@ -0,0 +1,27 @@
+@Tags(['golden'])
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:cm_sample/main.dart';
+void main() {
+  testWidgets('Verify initial UI and take screenshot',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+    await tester.pumpAndSettle();
+    // Normal golden test.
+    await expectLater(
+      find.byType(MyApp),
+      matchesGoldenFile('golden/initial_state.png'),
+    );
+  });
+
+  testWidgets('Verify incremented UI and take screenshot',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+    await tester.tap(find.byType(FloatingActionButton));
+    await tester.pump();
+    await expectLater(
+      find.byType(MyApp),
+      matchesGoldenFile('golden/incremented_state.png'),
+    );
+  });
+}
